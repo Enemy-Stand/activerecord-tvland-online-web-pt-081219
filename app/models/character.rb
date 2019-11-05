5c57a6abce1da9b301544ab8858d2a18c316d301
Joseph Sesti <sestijoey63@gmail.com>
2019-11-05 23:58:09 +0000

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,10 @@
 class Character < ActiveRecord::Base
-  belongs_to :actor, :shows
+  belongs_to :actor
+  belongs_to :show
+  
+  def say_that_thing_you_say
+    character = self.name
+    catchphrase = self.catchphrase
+    "#{character} always says: #{catchphrase}"
+  end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
 class Show < ActiveRecord::Base
-  has_many :actors, :characters
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+  
+  def actors_list
+    list = []
+    self.actors.map do |a|
+      list << a.full_name
+    end
+    list
+  end
 end
