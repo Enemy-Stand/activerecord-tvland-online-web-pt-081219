f93102678414d7bf0e2f3b20665842415ee3cdc5
Joseph Sesti <sestijoey63@gmail.com>
2019-11-05 23:28:35 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,4 +1,16 @@
 class Actor < ActiveRecord::Base
   has_many :characters
-  has_many :shows, through: :characters
+  has_many :show, through: :characters
+  
+  def full_name
+    self.first_name + " " + self.last_name
+  end
+  
+  def list_roles
+    self.characters.map do |c|
+      character = c.name
+      show = c.show.name
+      "#{character} - #{show}"
+    end
+  end
 end
